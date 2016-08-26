a158b430f956a3d7fd65a02cbf1ddbc13243e3ed
Benjamin Poon <benjamintpoon@gmail.com>
2016-08-26 11:21:23 -0500

    Remove hardcoded values from shaderProgram. gl_Position, gl_PointSize, and gl_Color are all passed in now.

--- webgl/script.js
+++ webgl/script.js
@@ -4,7 +4,9 @@ var gl,
 
 initGL();
 createShaders();
+createVertices();
 draw();
+
 
 function initGL() {
     var canvas = document.getElementById("canvas");
@@ -15,9 +17,11 @@ function initGL() {
 
 function createShaders() {
     var vs = "";
+    vs += "attribute vec4 coords;";
+    vs += "attribute float pointSize;";
     vs += "void main(void) {";
-    vs += "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);";
-    vs += "  gl_PointSize = 10.0;";
+    vs += "  gl_Position = coords;";
+    vs += "  gl_PointSize = pointSize;";
     vs += "}";
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -25,8 +29,10 @@ function createShaders() {
     gl.compileShader(vertexShader);
 
     var fs = "";
+    fs += "precision mediump float;";
+    fs += "uniform vec4 color;";
     fs += "void main(void) {";
-    fs += "  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);";
+    fs += "  gl_FragColor = color;";
     fs += "}";
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -38,6 +44,17 @@ function createShaders() {
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
+}
+
+function createVertices() {
+    var coords = gl.getAttribLocation(shaderProgram, "coords");
+    gl.vertexAttrib3f(coords, 0.5, 0, 0);
+
+    var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
+    gl.vertexAttrib1f(pointSize, 100);
+
+    var color = gl.getUniformLocation(shaderProgram, "color");
+    gl.uniform4f(color, 0, 1, 2, 1);
 }
 
 function draw() {
