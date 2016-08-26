f399b1eaa331aceb63abfb282703b393d43d6e08
Benjamin Poon <benjamintpoon@gmail.com>
2016-08-26 11:31:53 -0500

    using a vertex buffer to pass multiple points to shaderProgram

--- webgl/script.js
+++ webgl/script.js
@@ -1,6 +1,7 @@
 
 var gl,
-    shaderProgram;
+    shaderProgram,
+    vertices;
 
 initGL();
 createShaders();
@@ -10,6 +11,7 @@ draw();
 
 function initGL() {
     var canvas = document.getElementById("canvas");
+    console.log(canvas);
     gl = canvas.getContext("webgl");
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1, 1, 1, 1);
@@ -47,17 +49,30 @@ function createShaders() {
 }
 
 function createVertices() {
+    vertices = [
+        -0.9, -0.9, 0.0,
+        0.9, -0.9, 0.0,
+        0.0, 0.9, 0.0
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     var coords = gl.getAttribLocation(shaderProgram, "coords");
-    gl.vertexAttrib3f(coords, 0.5, 0, 0);
+    // gl.vertexAttrib3f(coords, 0.5, 0, 0);
+    gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coords);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
-    gl.vertexAttrib1f(pointSize, 100);
+    gl.vertexAttrib1f(pointSize, 10);
 
     var color = gl.getUniformLocation(shaderProgram, "color");
-    gl.uniform4f(color, 0, 1, 2, 1);
+    gl.uniform4f(color, 1, 1, 0, 1);
 }
 
 function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
