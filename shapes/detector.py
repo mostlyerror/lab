c926e7514b797843ea25c59108979e2f10c61b22
Benjamin Poon <benjamintpoon@gmail.com>
2016-08-26 22:39:31 -0500

    Slightly better CP curve detection, but might want to pursue another technique. Left some examples for sharing

--- shapes/detector.py
+++ shapes/detector.py
@@ -9,18 +9,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", help="path to image file")
 args = vars(parser.parse_args())
 
-print args
-
 image = cv2.imread(args["image"])
+truncated_file_name = args["image"].split(".")[0]
 
 def flash(image):
 	cv2.imshow("image", image)
 	cv2.waitKey(0)
 
-
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("%s-gray.jpg" % truncated_file_name, gray)
 
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imwrite("%s-blurred.jpg" % truncated_file_name, blurred)
 
 '''
 experiment with a good start value for threshold. 205?
@@ -47,7 +47,8 @@ for img in imgs:
 
 # _, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_TOZERO)
 _, thresh = cv2.threshold(blurred, 230, 255, cv2.THRESH_BINARY)
-flash(thresh)
+# flash(thresh)
+cv2.imwrite("%s-thresh.png" % truncated_file_name, thresh)
 
 # find contours in thresholded image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -55,18 +56,22 @@ cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPL
 # cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 print "%d contours found" % len(cnts[0])
 
-'''
+
+i = 0
 for c in cnts[0]:
 	# compute center
-	M = cv2.moments(c)
-	cX = int(M["m10"] / M["m00"])
-	cY = int(M["m01"] / M["m00"])
+	# M = cv2.moments(c)
+	# cX = int(M["m10"] / M["m00"])
+	# cY = int(M["m01"] / M["m00"])
 	# draw contour and center of the shape on the image
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 	#cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
 	#cv2.putText(image, "center", (cX-20, cY-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
 	# show the image
-	cv2.imshow("image", image)
+	i+=1
+	name = "%s-%d.png" % (truncated_file_name, i)
+	cv2.imshow(name, image)
+	cv2.imwrite(name, image)
 	cv2.waitKey(0)
-'''
+
