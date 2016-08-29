9af272381b33e3c654e946e03c24c7b41b955eb4
Benjamin Poon <benjamintpoon@gmail.com>
2016-08-29 17:39:51 -0500

    rotation for polygons implemented via model view matrix and gl-matrix.js lib

--- webgl/script.js
+++ webgl/script.js
@@ -72,7 +72,23 @@ function initShaders() {
 
 }
 
-var pMatrix = mat4.create(), mvMatrix = mat4.create();
+var pMatrix = mat4.create(), 
+    mvMatrix = mat4.create();
+var mvMatrixStack = [];
+
+function mvPushMatrix() {
+    var copy = mat4.create();
+    //mat4.set(mvMatrix, copy);
+    mat4.copy(copy, mvMatrix);
+    mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+        throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
 
 function setMatrixUniforms() {
     gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
@@ -135,6 +151,12 @@ function initBuffers () {
     squareVertexColorBuffer.numItems = 4;
 }
 
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+var rTri = 0;
+var rSquare = 0;
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -144,6 +166,11 @@ function drawScene() {
     mat4.identity(mvMatrix);
 
     mat4.translate(mvMatrix, mvMatrix, [-1.5, 0.0, -7.0]);
+
+    mvPushMatrix();
+    //mat4.rotate(mvMatrix, degToRad(rTri), [0, 1, 0]);
+    mat4.rotate(mvMatrix, mvMatrix, degToRad(rTri), [0, 1, 0]);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -152,6 +179,11 @@ function drawScene() {
 
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+    mvPopMatrix();
+
+    mvPushMatrix();
+    mat4.rotate(mvMatrix, mvMatrix, degToRad(rSquare), [1, 0, 0]);
 
     mat4.translate(mvMatrix, mvMatrix, [3.0, 0.0, 0.0]);
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
@@ -162,8 +194,26 @@ function drawScene() {
 
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+
+    mvPopMatrix();
 }
 
+var lastTime = 0;
+function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+        var elapsed = timeNow - lastTime;
+        rTri += (90 * elapsed) / 1000.0;
+        rSquare += (75 * elapsed) / 1000.0;
+    }
+    lastTime = timeNow;
+}
+
+function tick() {
+    requestAnimationFrame(tick);
+    drawScene();
+    animate();
+}
 
 function webGLStart () {
     var canvas = document.getElementById('canvas');
@@ -174,6 +224,7 @@ function webGLStart () {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
-    drawScene();
+    // drawScene();
+    tick();
 }
 
