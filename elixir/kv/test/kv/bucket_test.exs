d8e384f10043b49e6dc5487369158f452a28c878
Benjamin Poon <benjamintpoon@gmail.com>
2016-09-03 15:29:42 -0500

    delete for buckets

--- elixir/kv/test/kv/bucket_test.exs
+++ elixir/kv/test/kv/bucket_test.exs
@@ -12,4 +12,11 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes key and returns value", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 1)
+    assert KV.Bucket.get(bucket, "eggs") == 1
+    assert KV.Bucket.delete(bucket, "eggs") == 1
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
 end
