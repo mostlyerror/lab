5bb9d97ddded39a05c75b366a9866c164d1c5f0b
Benjamin Poon <benjamintpoon@gmail.com>
2016-09-03 13:02:46 -0500

    get rid of Number(x) bugs. sim.tick() retrieves coords of unhappy actors. prepared to do relocation next.

--- schelling/sim.js
+++ schelling/sim.js
@@ -11,12 +11,12 @@ class Grid {
 		let out = "",
 			row,
 			cells = this.cells;
-		for (var r in cells) {
+		for (var r = 0; r < cells.length; r++) {
 			if (Array.isArray(cells[r])) {
-				out += "\n";
+				out += "\n\n";
 			}
 			row = cells[r];
-			out += row.map(function (e) { return e.tribe; }).join(' ');
+			out += row.map(function (e) { return e.tribe; }).join("\t");
 		}
 		return out;
 	}
@@ -46,6 +46,21 @@ class Grid {
 		return this.cells = res;
 	}
 
+	get_empty () {
+		let empty = [],
+			r, cell;
+		for (var y = 0; y < this.cells.length; y++) {
+			r = this.cells[y].length;
+			for (var x = 0; x < r; x++) {
+				cell = this.cells[y][x];
+				if (cell.tribe === null) {
+					empty.push([x, y]);
+				}
+			}
+		}
+		return empty;
+	}
+
 	get_cell (x, y) {
 		return this.cells[y][x];
 	}
@@ -71,10 +86,6 @@ class Grid {
 			[-1, 0], [1, 0],
 			[-1, 1], [0, 1], [1, 1]
 		];
-	}
-
-	get EMPTY () {
-		return "."
 	}
 }
 
@@ -103,27 +114,49 @@ class Sim {
 		this.grid.cells = this.grid.cells_2d(this.grid.shuffle(arr));
 	}
 
-	is_unhappy (tribe, neighbors) {
-		return true;
+	is_unhappy (house, neighbors) {
+		// todo(ben): make threshold's have variation per household
+		// compare house's thresh vs  # of like-neighbors / total
+		// if thresh > ratio, no move
+		// else mark for moving
+
+		let n_neighbors = neighbors.filter((e) => { return e !== null}).length,
+			like = neighbors.filter((e) => { return e === house.tribe }),
+			ratio = like.length / n_neighbors;
+
+		console.log(house, neighbors, ratio, ratio < this.similarity_threshold);
+		return (ratio < this.similarity_threshold);
 	}
 
 	tick () {
-		let cell, neighbors;
 
-		for (var y in grid.cells) {
-			for (var x in grid.cells[y]) {
-				cell = grid.get_cell(x, y);
-				neighbors = grid.get_neighbors(Number(x), Number(y))
-					.map((e) => { return e.tribe })
-				    .filter((e) => { return e !== null });
-				console.log(neighbors);
+		// determine which houses are unhappy
+		let house, neighbors,
+			unhappy = [];
+		for (var y = 0; y < grid.cells.length; y++) {
+			for (var x = 0; x < grid.cells[y].length; x++) {
+				house = grid.get_cell(x, y);
+				if (house.tribe === null) break;
+				neighbors = grid.get_neighbors(x, y).map((e) => { return e.tribe })
+				    //.filter((e) => { return e !== null });
+				if (this.is_unhappy(house, neighbors)) {
+					console.log(`house ${x}, ${y} is unhappy!`);
+					unhappy.push([x,y,house.tribe]);
+				}
 			}
 		}
+		this.grid.shuffle(unhappy);
+		unhappy.forEach((e) => {
+			// todo(ben): make moves more intelligent, randomly for now. what's a better model?
+		});
+		console.log('unhappy: ',unhappy);
 	}
 }
 
-var grid = new Grid(10, 10);
-var sim = new Sim(grid, 2, 0.3);
+var grid = new Grid(4, 3);
+var sim = new Sim(grid, 2, 0.2, 0.3);
 console.log(grid.toString());
 sim.tick();
+
+console.log(grid.get_empty());
 
