96c31d238c75cf8f0bb003b3f7de4ea9f0ae8180
Benjamin Poon <benjamintpoon@gmail.com>
2016-09-22 18:14:10 -0500

    scrapped emurb for a js implementation. learning how cpus fetch, decode, dispatch, and execute instructions!

--- emurb/mmu.rb
+++
@@ -1,7 +0,0 @@
-class MMU
-  MEMORY_SIZE = 65536 # bytes
-
-  def initialize
-    @memory = Array.new MEMORY_SIZE, 0
-  end
-end
--- emurb/ppu.rb
+++
@@ -1,9 +0,0 @@
-class PPU
-  FRAMEBUFFER_SIZE = 23_040 # 160 & 144 (screen size)
-
-  def initialize
-    @framebuffer = Array.new FRAMEBUFFER_SIZE, 0
-    @mode = :vertical_blank
-    @modeclock = 0
-  end
-end
