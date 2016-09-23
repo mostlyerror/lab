ed76cd66f72f3d8cdb2ac29a21cb99519989b043
Benjamin Poon <benjamintpoon@gmail.com>
2016-09-23 15:15:44 -0500

    collision bug

--- pong/pong.js
+++ pong/pong.js
@@ -7,7 +7,9 @@ var canvas,
     ballX, ballY, ballSpeedX = 6, ballSpeedY = 9,
     framesPerSecond = 30,
     leftPlayerPoints = rightPlayerPoints = 0,
-    leftPaddleY, rightPaddleY;
+    leftPaddleY, rightPaddleY,
+    lptop, lpbot, rptop, rpbot,
+    scoreString;
 
 function reset () {
     clearInterval(interval);
@@ -16,41 +18,38 @@ function reset () {
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    ballX = canvas.width/2 - ballSize/2,
-    ballY = canvas.height/2 - ballSize/2;
-    leftPaddleY = rightPaddleY = canvas.height/2;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+    rightPaddleY = rightPaddleY = canvas.height/2;
 
     interval = setInterval(function () {
         move();
         draw();
     }, 1000/framesPerSecond);
+    
 };
 
-
 function move () {
+    lptop = leftPaddleY - paddleHeight/2;
+    lpbot = leftPaddleY + paddleHeight/2;
+
+    rptop = rightPaddleY - paddleHeight/2;
+    rpbot = rightPaddleY + paddleHeight/2;
 
     // left paddle collision check
-    var lptop = leftPaddleY - paddleHeight/2;
-    var lpbot = leftPaddleY + paddleHeight/2;
-
-    if (ballX >= canvas.width-paddleWidth) {
+    if ((ballX - ballSize/2) < paddleWidth) {
         ballSpeedX = -ballSpeedX;
         if (!((lptop < ballY) && (lpbot > ballY))) {
             leftPlayerPoints++;
-            //console.log('Score | ', leftPlayerPoints, rightPlayerPoints);
             reset();
         }
     }
 
     // right paddle collision check
-    var rptop = rightPaddleY - paddleHeight/2;
-    var rpbot = rightPaddleY + paddleHeight/2;
-
-    if (ballX <= paddleWidth) {
+    if ((ballX + ballSize/2) > canvas.width-paddleWidth) {
         ballSpeedX = -ballSpeedX;
         if (!((rptop < ballY) && (rpbot > ballY))) {
             rightPlayerPoints++;
-            //console.log('Score | ', leftPlayerPoints, rightPlayerPoints);
             reset();
         }
     }
@@ -64,25 +63,26 @@ function move () {
 }
 
 function draw () {
+    // clear
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = 'white';
-
     // paddles
-    context.fillRect(0, canvas.height/2 - paddleHeight/2, paddleWidth, paddleHeight);
+    context.fillStyle = 'white';
+    context.fillRect(0, leftPaddleY, paddleWidth, paddleHeight);
     context.fillRect(canvas.width-paddleWidth, canvas.height/2 - paddleHeight/2, paddleWidth, paddleHeight);
 
     //scoreboard
     context.font = "40px Verdana";
     context.textAlign = "center";
-    var scoreString = leftPlayerPoints + " - " + rightPlayerPoints;
+    scoreString = leftPlayerPoints + " - " + rightPlayerPoints;
     context.fillText(scoreString, canvas.width/2, 50);
 
     //ball
     context.fillStyle = 'red';
     context.fillRect(ballX, ballY, ballSize, ballSize);
-
 }
 
+document.onmousemove = function (e) { leftPaddleY = e.clientY - paddleHeight/2; };
 window.onload = reset;
+
