cb926f63106575a67468df9c4b7aec8da1f0a15a
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-01 02:14:53 -0500

    prettier index

--- gorails/books/db/seeds.rb
+++ gorails/books/db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-orson = Author.create({
-  name: 'Orson Scott Card'
-})
-
-Book.create({
-  title: "Ender's Game",
-  published_at: nil,
-  available: true,
-  author: orson
-});
+Book.create([
+  {
+    title: "Ender's Game",
+    description: "Child hero Ender Wiggin must fight a desperate battle against a deadly alien race if mankind is to survive."
+  },
+  {
+    title: "The Stranger",
+    description: "For Meursault, this is an insult to his reason and a betrayal of his hopes; for Camus it encapsulates the absurdity of life."
+  }
+]);
