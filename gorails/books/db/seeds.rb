817556f5fb131f3e775f0e6783c04ba480c2e8c6
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-01 03:05:51 -0500

    +will_paginate, will_paginate-bootstrap

--- gorails/books/db/seeds.rb
+++ gorails/books/db/seeds.rb
@@ -14,5 +14,55 @@ Book.create([
   {
     title: "The Stranger",
     description: "For Meursault, this is an insult to his reason and a betrayal of his hopes; for Camus it encapsulates the absurdity of life."
+  },
+  {
+    title: "The Curious Incident of the Dog in the Night-time",
+    description: "This improbable story of Christopher's quest to investigate the suspicious death of a neighborhood dog makes for one of the most captivating, unusual, and widely heralded novels in recent years."
+  },
+  {
+    title: "Guns, Germs, and Steel: The Fates of Human Societies",
+    description: "The author dismantles racially based theories of human history by revealing the environmental factors he feels are responsible for history's broadest patterns"
+  },
+  {
+    title: "A People's History of the United States",
+    description: "A classic since its original landmark publication in 1980, Howard Zinn’s A People’s History of the United States is the first scholarly work to tell America’s story from the bottom up—from the point of view of, and in the words of, America’s women, factory workers, African Americans, Native Americans, working poor, and immigrant laborers. From Columbus to the Revolution to slavery and the Civil War—from World War II to the election of George W. Bush and the “War on Terror”—A People’s History of the United States is an important and necessary contribution to a complete and balanced understanding of American history."
+  },
+  {
+    title: "The Count of Monte Cristo",
+    description: "Sent to prison on a false accusation in 1815, Edmond Dantes escapes many years later and finds a treasure which he uses to exact his revenge."
+  },
+  {
+    title: "Da Vinci Code",
+    description: "One of the bestselling novels of all time, Dan Brown's The Da Vinci Code has intrigued and thrilled millions of readers around the world. Now all the artwork, symbols, architecture, and historic locations—over 160 images—are beautifully compiled in this full-color collector's edition.
+
+    A mind-bending code hidden in the works of Leonardo da Vinci. A desperate race through the cathedrals and castles of Europe. An astonishing truth concealed for centuries… unveiled at last.
+
+    While in Paris on business, Harvard symbologist Robert Langdon receives an urgent late-night phone call. The elderly curator of the Louvre has been murdered inside the museum, a baffling cipher found near the body. As Langdon and a gifted French cryptologist, Sophie Neveu, sort through the bizarre riddles, they are stunned to discover a trail of clues hidden in the works of Da Vinci–clues visible for all to see and yet ingeniously disguised by the painter.
+
+    The stakes are raised when Langdon uncovers a startling link: the late curator was involved in the Priory of Sion—an actual secret society whose members included Sir Isaac Newton, Botticelli, Victor Hugo, and Da Vinci, among others. Langdon suspects they are on the hunt for a breathtaking historical secret, one that has proven through the centuries to be as enlightening as it is dangerous. In a frantic race through Paris, and beyond, Langdon and Neveu find themselves matching wits with a faceless powerbroker who appears to anticipate their every move. Unless they can decipher the labyrinthine puzzle, the Priory's secret—and an explosive ancient truth—will be lost forever.
+
+    Instantly catapulted to the top of the bestseller lists around the world, The Da Vinci Code is simultaneously lightning-paced, intelligent, and intricately layered with remarkable research and detail. From secrets embedded in the Mona Lisa and The Last Supper, to the symbols of ancient Egypt, to the architecture of landmarks such as the Louvre, Westminster Abbey, Rosslyn Chapel, and more, this fully illustrated collector's edition delivers the complete reading experience of Dan Brown's riveting novel—from the opening pages to the unpredictable and stunning conclusion."
+  },
+  {
+    title: "The Girl with the Dragon Tattoo",
+    description: "The disappearance forty years ago of Harriet Vanger, a young scion of one of the wealthiest families in Sweden, gnaws at her octogenarian uncle, Henrik Vanger. He is determined to know the truth about what he believes was her murder. He hires crusading journalist Mikael Blomkvist, recently at the wrong end of a libel case, to get to the bottom of Harriet's disappearance. Lisbeth Salander, a twenty-four-year-old, pierced, tattooed genius hacker, possessed of the hard-earned wisdom of someone twice her age--and a terrifying capacity for ruthlessness--assists Blomkvist with the investigation. This unlikely team discovers a vein of nearly unfathomable iniquity running through the Vanger family, an astonishing corruption at the highest echelon of Swedish industrialism--and a surprising connection between themselves.--From publisher description."
+  },
+  {
+    title: "Atonement",
+    description: "he novel opens on a sweltering summer day in 1935 at the Tallis family’s mansion in the Surrey countryside. Thirteen-year-old Briony has written a play in honor of the visit of her adored older brother Leon; other guests include her three young cousins -- refugees from their parent’s marital breakup -- Leon’s friend Paul Marshall, the manufacturer of a chocolate bar called “Amo” that soldiers will be able to carry into war, and Robbie Turner, the son of the family charlady whose brilliantly successful college career has been funded by Mr. Tallis. Jack Tallis is absent from the gathering; he spends most of his time in London at the War Ministry and with his mistress. His wife Emily is a semi-invalid, nursing chronic migraine headaches. Their elder daughter Cecilia is also present; she has just graduated from Cambridge and is at home for the summer, restless and yearning for her life to really begin. Rehearsals for Briony’s play aren’t going well; her cousin Lola has stolen the starring role, the twin boys can’t speak the lines properly, and Briony suddenly realizes that her destiny is to be a novelist, not a dramatist.
+
+    In the midst of the long hot afternoon, Briony happens to be watching from a window when Cecilia strips off her clothes and plunges into the fountain on the lawn as Robbie looks on. Later that evening, Briony thinks she sees Robbie attacking Cecilia in the library, she reads a note meant for Cecilia, her cousin Lola is sexually assaulted, and she makes an accusation that she will repent for the rest of her life.
+
+    The next two parts of Atonement shift to the spring of 1940 as Hitler’s forces are sweeping across the Low Countries and into France. Robbie Turner, wounded, joins the disastrous British retreat to Dunkirk. Instead of going up to Cambridge to begin her studies, Briony has become a nurse in one of London’s military hospitals. The fourth and final section takes place in 1999, as Briony celebrates her 77th birthday with the completion of a book about the events of 1935 and 1940, a novel called Atonement.
+
+    In its broad historical framework Atonement is a departure from McEwan’s earlier work, and he loads the story with an emotional intensity and a gripping plot reminiscent of the best nineteenth-century fiction. Brilliant and utterly enthralling in its depiction of childhood, love and war, England and class, the novel is a profoundly moving exploration of shame and forgiveness and the difficulty of absolution."
+  },
+  {
+    title: "Solar",
+    description: "Michael Beard is a Nobel prize-winning physicist whose best work is behind him. Trading on his reputation, he speaks for enormous fees, lends his name to the letterheads of renowned scientific institutions, and half-heartedly heads a government-backed initiative tackling global warming. While he coasts along in his professional life, Michael's personal life is another matter entirely. His fifth marriage is crumbling under the weight of his infidelities. But this time the tables are turned: His wife is having an affair, and Michael realizes he is still in love with her.
+
+    When Michael's personal and professional lives begin to intersect in unexpected ways, an opportunity presents itself in the guise of an invitation to travel to New Mexico. Here is a chance for him to extricate himself from his marital problems, reinvigorate his career, and very possibly save the world from environmental disaster. Can a man who has made a mess of his life clean up the messes of humanity?
+
+    A complex novel that brilliantly traces the arc of one man's ambitions and self-deceptions, Solar is a startling, witty, and stylish new work from one of the world's great writers."
   }
 ]);
