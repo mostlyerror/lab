cc84f01d9c316e6330a3fada7f36820e9bac7285
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-01 01:45:43 -0500

    adding bootstrap-sass gem, installing a navbar and enabling bootstrap javascript stuffs

--- gorails/blog/app/models/author.rb
+++
@@ -1,5 +0,0 @@
-class Author < ApplicationRecord
-  has_many :books
-
-  scope :available_books, ->() { joins(:books).merge(Book.available) }
-end
--- gorails/blog/app/models/book.rb
+++
@@ -1,6 +0,0 @@
-class Book < ApplicationRecord
-  belongs_to :author
-
-  scope :available, ->() { where available: true }
-  scope :unavailable, ->() { where available: [nil, false] }
-end
