34af56cc836e9680e50cb9b62344e0a5e85f6a54
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-01 16:24:58 -0500

    add image uploads via carrierwave

--- gorails/books/app/models/book.rb
+++ gorails/books/app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   extend FriendlyId
 
   friendly_id :title, use: :slugged
+
+  mount_uploader :cover_image, ImageUploader
 end
--- gorails/books/db/schema.rb
+++ gorails/books/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001073457) do
+ActiveRecord::Schema.define(version: 20161001202219) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20161001073457) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "slug"
+    t.string   "cover"
+    t.string   "cover_image"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
