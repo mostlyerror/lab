9ff3ea094fe90979f1cb862664f64bc68f600d14
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-01 17:07:49 -0500

    added devise and User model. Links to signup/login/logout in the navbar.

--- gorails/books/config/routes.rb
+++ gorails/books/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
+
+  devise_for :users
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'blog', to: 'blog#index'
-  get 'blog/:id', to: 'blog#show'
-  post 'blog', to: 'blog#create'
 end
