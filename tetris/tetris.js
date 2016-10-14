0d3e314b9992bdd7331eb7cfd3aea43be48bbfd2
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-14 00:48:00 -0500

    sweep arena and re-stack empty row on top when piece hits bottom.

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -4,11 +4,18 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-const matrix = [
-	[0, 0, 0],
-	[1, 1, 1],
-	[0, 1, 0],
-];
+function arenaSweep() {
+	outer: for (let y = arena.length -1; y > 0; --y) { // yavascript trick to continue on the outer loop!
+		for (let x = 0; x < arena[y].length; ++x) {
+			if (arena[y][x] === 0) {
+				continue outer;
+			}
+		}
+		const row = arena.splice(y, 1)[0].fill(0);
+		arena.unshift(row);
+		++y;
+	}
+}
 
 function collide(arena, player) {
 	const [m, o] = [player.matrix, player.pos];
@@ -34,6 +41,46 @@ function createMatrix(w, h) {
 	return matrix;
 }
 
+function createPiece(type) {
+	if (type === 'T') {
+		return [
+			[0, 0, 0],
+			[1, 1, 1],
+			[0, 1, 0],
+		];
+	} else if (type === 'O') {
+		return [
+			[2, 2],
+			[2, 2]
+		];
+	} else if (type === 'L') {
+		return [
+			[0, 3, 0],
+			[0, 3, 0],
+			[3, 3, 0]
+		];
+	} else if (type === 'I') {
+		return [
+			[0, 4, 0, 0],
+			[0, 4, 0, 0],
+			[0, 4, 0, 0],
+			[0, 4, 0, 0]
+		];
+	} else if (type === 'S') {
+		return [
+			[0, 5, 0],
+			[5, 5, 0],
+			[5, 0, 0],
+		];
+	} else if (type === 'Z') {
+		return [
+			[6, 6, 0],
+			[0, 6, 6],
+			[0, 0, 0]
+		];
+	}
+}
+
 function draw() {
 	context.fillStyle = '#000';
 	context.fillRect(0, 0, canvas.height, canvas.width);
@@ -46,7 +93,7 @@ function drawMatrix(matrix, offset) {
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value !== 0) {
-				context.fillStyle = 'red';
+				context.fillStyle = colors[value];
 				context.fillRect(x + offset.x, 
 								 y + offset.y, 
 								 1, 1);
@@ -71,7 +118,8 @@ function playerDrop() {
 		if (collide(arena, player)) {
 			player.pos.y--;
 			merge(arena, player);
-			player.pos.y = 0;
+			playerReset();
+			arenaSweep();
 		}
 		dropCounter = 0; // keep 1 second delay for auto-drop after manual drop
 }
@@ -80,6 +128,17 @@ function playerMove(dir) {
 	player.pos.x += dir;
 	if (collide(arena, player)) { 
 		player.pos.x -= dir;
+	}
+}
+
+function playerReset() {
+	const pieces = 'ILJOTSZ';
+	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+	player.pos.y = 0;
+	player.pos.x = (arena[0].length / 2 | 0)  - 
+				   (player.matrix[0].length / 2 | 0);
+	if (collide(arena, player)) {
+		arena.forEach(row => row.fill(0));
 	}
 }
 
@@ -135,10 +194,20 @@ function update(time = 0) {
 	window.requestAnimationFrame(update);
 }
 
+const colors = [
+	null,
+	'#ff0d72',
+	'#0dc2ff',
+	'#0dff72',
+	'#f538ff',
+	'#ff8e0d',
+	'#ffe138',
+	'#3877ff'
+];
+
 const player = {
 	pos: {x: 5, y: 5},
-	matrix: matrix
-
+	matrix: createPiece('T')
 };
 
 const arena = createMatrix(12, 20);
