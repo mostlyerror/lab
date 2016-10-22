909fb3401524ad17be0609a0e85ba9b79bc3a179
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-22 00:41:26 -0500

    messing with some o' dat Vue.js

---
+++ lynda/vuejs/app.js
@@ -0,0 +1,67 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'hey' },
+            { text: 'howdy' },
+            { text: 'allo!'}
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split(' ').reverse().join(' ')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        todos: [
+            { text: 'learn javascript' },
+            { text: 'learn vue' },
+            { text: 'build something awesome' }
+        ]
+    }
+});
