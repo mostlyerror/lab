494dafb393f302000713218fb28f71791505a4d2
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-26 23:45:16 -0500

    computed prop names

--- practical-es6/examples.js
+++ practical-es6/examples.js
@@ -1,5 +1,5 @@
 
-// Object Literals
+// OBJECT LITERALS
 //
 // prop value shortcuts
 var store = {};
@@ -16,4 +16,54 @@ function setItem(key, value) {
 
 function clear() {
     store = {};
+}
+
+
+// COMPUTED PROP NAMES
+
+//var expertise = 'journalism';
+//var person = {
+    //name: 'Sharon',
+    //age: 27
+//};
+//person[expertise] = {
+    //years: 5,
+    //interests: ['international', 'politics', 'internet']
+//};
+
+var expertise = 'journalism';
+var person = {
+    name: 'Sharon',
+    age: 27,
+    [expertise]: {
+        years: 5,
+        interests: ['international', 'politics', 'internet']
+    }
+};
+
+var grocery = {
+    id: 'bananas',
+    name: 'Bananas',
+    units: 6, 
+    price: 10,
+    currency: 'USD'
+};
+var groceries = {
+    [grocery.id]: grocery
+};
+
+
+//function getEnevelope (type, description) {
+    //var envelope = {
+        //data: {}
+    //};
+    //envelope[type] = description;
+    //return envelope;
+//}
+
+function getEnevelope (type, description) {
+    return {
+        data: {},
+        [type]: description
+    }
 }
