93bf36f8afd4c08829c56b530780d1aebb6de9db
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-27 14:27:34 -0500

    Parser builds AST from lexer tokens. Rigth now only contains a list of CallExpresions with their single number literal args.

--- compylerr/lexer.js
+++ compylerr/lexer.js
@@ -1,4 +1,5 @@
-const readFile = require('fs-readfile-promise');
+const readFile = require('fs-readfile-promise'),
+      chalk = require('chalk');
 
 function lexer (sourceBuffer) {
     return sourceBuffer
@@ -12,12 +13,63 @@ function lexer (sourceBuffer) {
         });
 }
 
+function parser (tokens) {
+    const AST = {
+        type: 'Drawing',
+        body: []
+    };
+
+    while (tokens.length > 0) {
+        const currentToken = tokens.shift();
+        if (currentToken.type === 'word') {
+
+            switch (currentToken.value) {
+                case 'Paper':
+                    const expr = {
+                        type: 'CallExpression',
+                        name: 'Paper',
+                        arguments: []
+                    };
+
+                    const num = parseNum(tokens.shift());
+                    expr.arguments.push(num);
+                    AST.body.push(expr);
+
+                break;
+
+                case 'Pen':
+                    // ...
+                break;
+
+                case 'Line':
+                    // ...
+                break;
+            }
+        }
+    }
+    return AST;
+}
+
+function parseNum (token) {
+    if (token.type !== 'number') throw(expr.name + ' node must be followed by a number.');
+    return {
+        type: 'NumberLiteral',
+        value: token.value
+    }
+}
+
+
+
+
 const file = process.argv.pop()
 
 readFile(file)
     .then(buffer => {
-        const lexed = lexer(buffer)
-        console.log(lexed);
+        const tokens = lexer(buffer)
+        console.log(tokens);
+
+        const parsed = parser(tokens);
+        console.log(parsed);
     })
     .catch(err => console.error(err.message));
 
