f32e3a548be8de0fc8fa342b8931fa0a89470110
Benjamin Poon <benjamintpoon@gmail.com>
2016-10-27 13:30:40 -0500

    Refactor to promise-based file reading with `fs-readfile-promise` :star: :clap:

--- compylerr/lexer.js
+++ compylerr/lexer.js
@@ -1,8 +1,9 @@
-//fs = require('fs')
-const reader = require('buffered-reader');
+const readFile = require('fs-readfile-promise');
 
-function lexer (code) {
-    return code.split(/\s+/) 
+function lexer (sourceBuffer) {
+    return sourceBuffer
+        .toString()
+        .split(/\s+/) 
         .filter(t => t.length > 0) // might not need this?
         .map((t) => {
             return isNaN(t)
@@ -11,18 +12,10 @@ function lexer (code) {
         });
 }
 
+readFile('lexer.js')
+    .then(buffer => {
+        const lexed = lexer(buffer)
+        console.log(lexed);
+    })
+    .catch(err => console.error(err.message));
 
-function lexMe (err, source) {
-    if (err) return console.log(err);
-
-    return lexer(source.toString());
-}
-
-//const lexed = fs.readFile('lexer.js', lexMe);
-//console.log(lexed);
-//
-new reader.DataReader ('lexer.js', { encoding: 'utf8' })
-    .on('error', (err) => console.log('err: ', err))
-    .on('line', (line) => console.log(lexMe(null, line)))
-    .on('end', () => console.log('EOF'))
-    .read();
