628dea25a5cac41cfba3d96dd4ebf4ee715f26c8
Benjamin Poon <benjamintpoon@gmail.com>
2017-03-13 23:23:51 -0500

    egghead react/redux click couner using the store

--- egghead-redux/reducer.js
+++ egghead-redux/reducer.js
@@ -1,5 +1,7 @@
-var expect = require('expect');
+//var expect = require('expect');
+//var Redux = require('redux');
 
+// reducer
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -11,24 +13,39 @@ const counter = (state = 0, action) => {
   }
 }
 
-expect(
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1);
+const { createStore } = Redux;
+const store = createStore(counter);
 
-expect(
-  counter(1, { type: 'INCREMENT' })
-).toEqual(2);
+const render = () => {
+  document.body.innerText = store.getState();
+}
+// redux calls whenever an action is dispatched
+store.subscribe(render);
+render();
 
-expect(
-  counter(2, { type: 'DECREMENT' })
-).toEqual(1);
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
 
-expect(
-  counter(1, { type: 'DECREMENT' })
-).toEqual(0);
 
-expect(
-  counter(0, { type: 'SOMETHING_ELSE' })
-).toEqual(0);
+//expect(
+  //counter(0, { type: 'INCREMENT' })
+//).toEqual(1);
 
-console.log('Test passed!');
+//expect(
+  //counter(1, { type: 'INCREMENT' })
+//).toEqual(2);
+
+//expect(
+  //counter(2, { type: 'DECREMENT' })
+//).toEqual(1);
+
+//expect(
+  //counter(1, { type: 'DECREMENT' })
+//).toEqual(0);
+
+//expect(
+  //counter(0, { type: 'SOMETHING_ELSE' })
+//).toEqual(0);
+
+//console.log('Test passed!');
