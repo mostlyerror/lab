3040efaa68aba5cd625b045736ac1e79a01e9160
Benjamin Poon <benjamintpoon@gmail.com>
2017-05-22 16:10:26 -0500

    learning about routes, query params, etc, in express

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
+
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
