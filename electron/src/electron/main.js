019a7bb2b9f816cfe5a78ce53d2c2212533b6caf
Ben Poon <benjamintpoon@gmail.com>
2025-01-24 14:42:36 -0600

    install and configure some electron & vite stuff

---
+++ electron/src/electron/main.js
@@ -0,0 +1,11 @@
+import {app, BrowserWindow} from 'electron'
+import path from 'path'
+
+app.on('ready', () => {
+  let mainWindow = new BrowserWindow({})
+  // can't juse use this: mainWindow.loadFile('dist-react/index.html')
+  // Because we don't actually know where the app is running from
+  // We also need to use node's path.join() because we don't know what backslash
+  // format the OS supports
+  mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+})
--- electron/vite.config.ts
+++ electron/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Tell vite we want to serve the files from the root, which is relative, not
+  // absolute '/'
+  base: "./",
   build: {
     outDir: 'dist-react',
   }
